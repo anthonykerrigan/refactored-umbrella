6973e622015b67ffd74e97181ceb9b7989bd455d
Anthony Kerrigan <kerrigan.anthony@gmail.com>
2022-11-29 18:27:30 +1000

    Starting work on responding to new users.

--- bot.py
+++ bot.py
@@ -12,10 +12,28 @@ client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    #guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f'{client.user} is connected to the following guild: '
+        f'{guild.name}(id: {guild.id})\n'
+        f'{client.user.name} has connected to Discord!'
     )
+
+@client.event
+async def on_member_join(member):
+    channels = client.get_all_channels
+    channel = channels['General']
+    await message.channel.send(
+        f'Hi {member.name}! Welcome to Kab\'s useless attempt at making a bot!'
+    )   
+
+
+@client.event
+async def on_message(message): 
+    if message.author == client.user:
+        return
+
+    
 
 client.run(TOKEN)
