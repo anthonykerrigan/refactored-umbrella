f702ec561fc61f0abd4716c6253761e18ac22e2f
Anthony Kerrigan <kerrigan.anthony@gmail.com>
2022-12-20 12:38:14 +1000

    Updates to parse JSON responses instead of Regex

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 # bot.py
 import os
-
+import requests
+import re
+import json
 import discord
 intents = discord.Intents.default()
 intents.members = True
@@ -11,6 +13,8 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+FACTS_API = os.getenv('FACTS_API_ID')
+BOT_PREFIX = os.getenv('BOT_PREFIX')
 
 client = discord.Client(intents=intents)
 
@@ -42,12 +46,26 @@ async def on_message(message):
         #print(message)
         await message.channel.send("Hello")
         print("THERE! I said Hi! Are you proud of me!?")
-
-
-#@client.event
-#async def on_message(message): 
-#    if message.author == client.user:
-#        return
+    if message.content.find('fact') != -1:
+        print("fact found")
+        limit = 1 
+        api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
+        response = requests.get(api_url, headers={'X-Api-Key': FACTS_API})
+        parsed = response.json()
+        if response.status_code == requests.codes.ok:
+            await message.channel.send("Did you know " + parsed[0]['fact'])
+            print("Okay random fact sent")
+        else:
+            print("Error:", response.status_code, response.text)
+    if message.content == "!joke":
+        limit = 1
+        api_url = 'https://api.api-ninjas.com/v1/jokes?limit={}'.format(limit)
+        response = requests.get(api_url, headers={'X-Api-Key': FACTS_API})
+        parsed = response.json()
+        if response.status_code == requests.codes.ok:
+            await message.channel.send(parsed[0]['joke'])
+        else:
+            print("Error:", response.status_code, response.text)
 
     
 
